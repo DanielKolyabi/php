cbd53de907a8cb72f474b670f7a42a897d2ef7c9
crasher307 <crasher307@gmail.com>
2024-05-27 15:55:15 +0500

    lesson 2

---
+++ laravel/app/resources/views/includes/header.blade.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-expand-sm navbar-light bg-light">
+    <a class="navbar-brand" href="#">{{config('app.name')}}</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="navbar-nav collapse navbar-collapse" id="navbarNav">
+        <a class="nav-item nav-link {{active_link('/')}}" href="{{route('home')}}">
+            {{__('Главная')}}
+        </a>
+        <a class="nav-item nav-link {{active_link('/userform')}}" href="{{route('userform')}}">
+            {{__('UserForm')}}
+        </a>
+    </div>
+</nav>
